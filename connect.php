5c16d32b5f322584e8c25433e7dd07bb6fd132e0
lvwerner <viniciushhardt@gmail.com>
2023-10-25 09:43:43 -0300

    update 25/10

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhostsql211.infinityfree.com";
-$usuario = "if0_35295512";
-$senha = "sabombinhas";
-$banco = "if0_35295512_sa_bombeiros";
+$host = "localhost";
+$usuario = "root";
+$senha = "root";
+$banco = "sa_bombeiros";
 
 $conexao = new mysqli($host, $usuario, $senha, $banco);
 
