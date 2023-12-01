5ed07e471cf81beb6062ad63f47ae396ca20b09e
lvwerner <viniciushhardt@gmail.com>
2023-12-01 07:34:18 -0300

    update final / deliver

--- buscar_ocorrencias.php
+++ buscar_ocorrencias.php
@@ -2,6 +2,12 @@
 <html>
 <head>
     <title>Dados da Tabela</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&display=swap" rel="stylesheet">
+
     <style>
         /* Estilos para a tabela */
         table {
@@ -49,6 +55,81 @@
     </style>
 </head>
 <body id="body">
+<nav id="navbarra" class="navbar navbar-expand-lg d-none d-lg-block">
+        <div class="container">
+            <a class="navbar-brand mb-0 h1" href="index.php">Ficha de Ocorrência</a>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Página Inicial</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="perfil.php">Perfil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.html">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="atendente.php">Atendimento</a>
+                </li>
+                <form action="logout.php" method="post">
+                    <input type="submit" name="logout" value="Sair" class="logout-button nav-link" />
+                </form>
+
+            </ul>
+        </div>
+    </nav>
+    <!-- Menu em offcanva -->
+    <!-- Breve explicação das classes
+                - d-lg-none: Indica ao Bootstrap que este menu offcanvas não irá aparecer em telas maiores que 992px.
+                - navbar-toggler: Indica ao Bootstrap que este é um botão de alternância.
+                - data-bs-toggle: Indica que este botão será utilizado para alternar a visibilidade do offcanvas.
+                - data-bs-target: Indica qual elemento terá sua visibilidade alternada.
+                - offcanvas-start: Indica que o menu offcanvas deve começar com seus elementos alinhados à esquerda da tela.
+                - aria-label: É utilizado para melhorar a acessibilidade da página da web.
+            -->
+    <div class="d-lg-none">
+
+        <nav class="navbar navbar-light bg-light">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">S.A. Bombeiros</a>
+            </div>
+        </nav>
+
+        <div class="offcanvas offcanvas-start" id="offcanvasNav">
+
+            <div class="offcanvas-header">
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Fechar"></button>
+            </div>
+
+            <div class="offcanvas-body">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Página Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="perfil.php">Perfil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.html">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="atendente.php">Atendimento</a>
+                    </li>
+                </ul>
+                <form action="logout.php" method="post">
+                    <input type="submit" name="logout" value="Sair" class="logout-button nav-link" />
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+    </header>
+    </div>
     <?php
     $servername = "localhost";
     $username = "root";
@@ -102,5 +183,8 @@
             }
         }
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
 </html>
